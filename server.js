ae2524ab07a4e7f6326a73d55dd6bba0aef00069
cybermaxi7 <maximusagbe@gmail.com>
2023-12-13 00:27:11 +0100

    added custom routes

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const jsonServer = require("json-server");
+const jsonServer = require('json-server');
+const customRoutes = require('./customRoutes');
 
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-
+// Custom route for exercise search
+server.use('/exercises', customRoutes);
 
 server.use(middlewares);
 server.use(router);
 
 const PORT = 3000;
 server.listen(PORT, () => {
-    console.log(`JSON Server is running on port ${PORT}`);
+  console.log(`JSON Server is running on port ${PORT}`);
 });
