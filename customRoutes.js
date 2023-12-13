b3f150dacef8f305b65c226d5ca37f00372cc49b
cybermaxi7 <maximusagbe@gmail.com>
2023-12-13 06:57:39 +0100

    added an s to the exercises name keyword in my db.json

--- customRoutes.js
+++ customRoutes.js
@@ -1,14 +1,22 @@
 module.exports = (req, res, next) => {
-    const db = require('./db.json');
-    const { name } = req.query;
-  
-    if (name) {
-      const filteredExercises = db.exercises.filter(exercise =>
-        exercise.name.toLowerCase().includes(name.toLowerCase())
-      );
-      res.json(filteredExercises);
-    } else {
-      next();
-    }
-  };
-  
+  try {
+      const db = require('./db.json');
+      const { name } = req.query;
+
+      console.log('db:', db);
+      console.log('name:', name);
+
+      if (db && db.exercises && name) {
+          const filteredExercises = db.exercises.filter(exercise =>
+              exercise.name.toLowerCase().includes(name.toLowerCase())
+          );
+          console.log('filteredExercises:', filteredExercises);
+          res.json(filteredExercises);
+      } else {
+          next();
+      }
+  } catch (error) {
+      console.error('Error in custom middleware:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
