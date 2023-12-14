1429192720c47ef61ed971a49274dde0500f2c93
cybermaxi7 <maximusagbe@gmail.com>
2023-12-14 10:58:30 +0100

    chaned cors from all to a specific origin

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const jsonServer = require('json-server');
-const customRoutes = require('./customRoutes');
-const cors = require('cors');
+const jsonServer = require("json-server");
+const customRoutes = require("./customRoutes");
+const cors = require("cors");
 
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-// Enable cors for routes
-server.use(cors());
+// Enable CORS for a specific origin
+server.use(cors({ origin: "https://localhost:5173" }));
 // Custom route for exercise search
-server.use('/exercises', customRoutes);
+server.use("/exercises", customRoutes);
 
 server.use(middlewares);
 server.use(router);
 
 const PORT = 3000;
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+    console.log(`JSON Server is running on port ${PORT}`);
 });
