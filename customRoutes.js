150012dc293abb8eee4a6c68214f58a1359c90b5
cybermaxi7 <maximusagbe@gmail.com>
2023-12-17 17:38:00 +0100

    got id from the query data

--- customRoutes.js
+++ customRoutes.js
@@ -1,37 +1,36 @@
 module.exports = (req, res, next) => {
-    try {
-      const db = require('./db.json');
-      const { name, limit } = req.query;
-  
-      if (db && db.exercises) {
-        let filteredExercises = db.exercises;
+  try {
+    const db = require('./db.json');
+    const { name, id, limit } = req.query;
 
-        if (id) {
-          // Search by ID
-          const exerciseById = db.exercises.find(exercise => exercise.id === id);
-          res.json(exerciseById ? [exerciseById] : []);
-        }
-  
-        if (name) {
-          // Filter by name
-          filteredExercises = filteredExercises.filter(exercise =>
-            exercise.name.toLowerCase().includes(name.toLowerCase())
-          );
-        }
-  
-        if (limit) {
-          // Limit the number of results
-          const limitValue = parseInt(limit, 10);
-          filteredExercises = filteredExercises.slice(0, limitValue);
-        }
-  
-        res.json(filteredExercises);
-      } else {
-        next();
+    if (db && db.exercises) {
+      let filteredExercises = [...db.exercises]; // Use spread to create a copy
+
+      if (id) {
+        // Search by ID
+        const exerciseById = db.exercises.find(exercise => exercise.id === id);
+        return res.json(exerciseById ? [exerciseById] : []);
       }
-    } catch (error) {
-      console.error('Error in custom middleware:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+
+      if (name) {
+        // Filter by name
+        filteredExercises = filteredExercises.filter(exercise =>
+          exercise.name.toLowerCase().includes(name.toLowerCase())
+        );
+      }
+
+      if (limit) {
+        // Limit the number of results
+        const limitValue = parseInt(limit, 10);
+        filteredExercises = filteredExercises.slice(0, limitValue);
+      }
+
+      return res.json(filteredExercises);
+    } else {
+      next();
     }
-  };
-  
+  } catch (error) {
+    console.error('Error in custom middleware:', error);
+    res.status(500).json({ error: `Internal Server Error: ${error.message}` });
+  }
+};
