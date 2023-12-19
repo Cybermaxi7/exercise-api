0f983c24783d9c6e187008d67bf4b1b0ee9b6a6a
cybermaxi7 <maximusagbe@gmail.com>
2023-12-19 12:29:19 +0100

    fixed the performance issues by taking out db from every render instance

--- customRoutes.js
+++ customRoutes.js
@@ -1,36 +1,42 @@
-module.exports = (req, res, next) => {
-  try {
-    const db = require('./db.json');
-    const { name, id, limit } = req.query;
+// customRoutes.js
+const db = require('./db.json');
 
-    if (db && db.exercises) {
-      let filteredExercises = [...db.exercises]; // Use spread to create a copy
+module.exports = () => {
+  return (req, res, next) => {
+    try {
+      const { name, id, limit } = req.query;
+      console.log('Received id:', id); // Add this line for debugging
 
-      if (id) {
-        // Search by ID
-        const exerciseById = db.exercises.find(exercise => exercise.id === id);
-        return res.json(exerciseById ? [exerciseById] : []);
+
+      if (db && db.exercises) {
+        let filteredExercises = [...db.exercises];
+
+        if (id) {
+          // Search by ID
+          const exerciseById = db.exercises.find(exercise => exercise.id === String(id));
+          return res.json(exerciseById ? [exerciseById] : []);
+        }
+
+        if (name) {
+          // Filter by name
+          filteredExercises = filteredExercises.filter(exercise =>
+            exercise.name.toLowerCase().includes(name.toLowerCase())
+          );
+        }
+
+        if (limit) {
+          // Limit the number of results
+          const limitValue = parseInt(limit, 10);
+          filteredExercises = filteredExercises.slice(0, limitValue);
+        }
+
+        return res.json(filteredExercises);
+      } else {
+        next();
       }
-
-      if (name) {
-        // Filter by name
-        filteredExercises = filteredExercises.filter(exercise =>
-          exercise.name.toLowerCase().includes(name.toLowerCase())
-        );
-      }
-
-      if (limit) {
-        // Limit the number of results
-        const limitValue = parseInt(limit, 10);
-        filteredExercises = filteredExercises.slice(0, limitValue);
-      }
-
-      return res.json(filteredExercises);
-    } else {
-      next();
+    } catch (error) {
+      console.error('Error in custom middleware:', error);
+      res.status(500).json({ error: `Internal Server Error: ${error.message}` });
     }
-  } catch (error) {
-    console.error('Error in custom middleware:', error);
-    res.status(500).json({ error: `Internal Server Error: ${error.message}` });
-  }
+  };
 };
--- server.js
+++ server.js
@@ -1,21 +1,50 @@
-const jsonServer = require("json-server");
-const customRoutes = require("./customRoutes");
-const cors = require("cors");
+// server.js
+const jsonServer = require('json-server');
+const customRoutes = require('./customRoutes');
+const cors = require('cors');
+const fs = require('fs').promises;
 
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-// Enable CORS for a specific origin
-const allowedOrigins = ["http://localhost:5173", "https://exercise-hub.vercel.app" ]
+// Enable CORS for all origins during development
+const allowedOrigins = process.env.NODE_ENV === 'production'
+  ? ['https://exercise-hub.vercel.app']
+  : ['*'];
 server.use(cors({ origin: allowedOrigins }));
+
 // Custom route for exercise search
-server.use("/exercises", customRoutes);
+server.use('/exercises', customRoutes());
+
+// Handle preflight requests
+server.options('*', cors());
 
 server.use(middlewares);
 server.use(router);
 
 const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`JSON Server is running on port ${PORT}`);
-});
+
+async function loadDatabase() {
+  try {
+    const content = await fs.readFile('./db.json', 'utf-8');
+    return JSON.parse(content);
+  } catch (error) {
+    console.error('Error loading database:', error);
+    throw error;
+  }
+}
+
+const dbPromise = loadDatabase();
+
+// Start the server
+dbPromise
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`JSON Server is running on port ${PORT}`);
+    });
+  })
+  .catch(error => {
+    console.error('Error starting the server:', error);
+    process.exit(1);
+  });
